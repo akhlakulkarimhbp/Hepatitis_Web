fdc00cac45767805a6f84a58910d49f5620527f2
Akhlakul Karim HBP <akhlakulkarimhbp110214@gmail.com>
2021-04-13 16:30:49 +0700

    login dan register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,31 +1,45 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-  <link rel="stylesheet" href="{{asset('css/style-login.css')}}">
-  <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
-  <title>Hepatitis A</title>
+    <link rel="stylesheet" href="{{ asset('css/style-login.css') }}">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Hepatitis A</title>
 </head>
 
 <body>
     <div class="main">
-      <p class="sign" align="center">LOGIN</p>
-      <form method="POST" action="{{ route('login')}}" class="form1">
-      @csrf
-      <input id="email" class="email" type="email" align="center" placeholder="Email">
-      <input id="password" class="pass" type="password" align="center" placeholder="Password">
-      <button type="submit" class="submit" align="center">MASUK</button>   
-      </form>
-      <br>
-      <button type="submit" class="submit" align="center">REGISTER</button> 
-      </div>
-      <div class="main1">
-        <img class="gambar1" src="{{asset('css/first.png')}}">
-              <p class="kata2"> SELAMAT DATANG  DI WEBSITE TINGKAT KERAWANAN</p>
-              <p class="kata3"> HEPATITIS A DI KABUPATEN PACITAN</p>
-              <p class="kata4"> - HEPATITIC A RATE -</p>
-        </div>
+        <p class="sign" align="center">LOGIN</p>
+        <form method="POST" action="{{ route('login') }}" class="form1 needs-validation"
+            novalidate="">
+            @csrf
+            <div class="form-group">
+                <input id="email" class="email form-control" type="email" placeholder="Email"
+                    name="email" value="{{ old('email') }}" required>
+            </div>
+
+            <div class="form-group">
+                <input id="password" class="pass form-control" type="password" placeholder="Password"
+                    name="password" required autocomplete="current-password">
+            </div>
+            <div class="form-group">
+                <button type="submit" class="submit">
+                    Masuk
+                </button>
+            </div>
+            <br>
+            <div class="mt-5 text-center" align="center">
+                Belum punya akun? <a href="{{route('register')}}">Daftar</a>
+            </div>
+        </form>
+    </div>
+    <div class="main1">
+        <img class="gambar1" src="{{ asset('css/first.png') }}">
+        <p class="kata2"> SELAMAT DATANG DI WEBSITE TINGKAT KERAWANAN</p>
+        <p class="kata3"> HEPATITIS A DI KABUPATEN PACITAN</p>
+        <p class="kata4"> - HEPATITIC A RATE -</p>
+    </div>
 </body>
 
 </html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,76 +1,57 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+<head>
+    <link rel="stylesheet" href="{{ asset('css/style-login.css') }}">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Hepatitis A</title>
+</head>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+<body>
+    <div class="main">
+        <p class="regis" align="center">REGISTER</p>
+        <form method="POST" action="{{ route('register') }}" class="form1">
+            @csrf
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+            <div class="form-group">
+                <input id="name" class="email form-control" type="text" placeholder="Full Name" name="name"
+                    value="{{ old('name') }}" required>
             </div>
-        </div>
+
+            <div class="form-group">
+                <input id="email" class="email form-control" type="email" placeholder="Email" name="email"
+                    value="{{ old('email') }}" required>
+            </div>
+
+            <div class="form-group">
+                <input id="password" class="pass form-control" type="password" placeholder="Password" name="password"
+                    required autocomplete="new-password">
+            </div>
+
+            <div class="form-group">
+                <input id="password-confirm" class="pass form-control" type="password"
+                    placeholder="Password Confirmation" name="password_confirmation" required
+                    autocomplete="new-password">
+            </div>
+
+            <div class="form-group">
+                <button type="submit" class="submit">
+                    Register
+                </button>
+            </div>
+            <br>
+            <div class="mt-5 text-center" align="center">
+                Sudah punya akun? <a href="{{ route('login') }}">Masuk</a>
+            </div>
+        </form>
     </div>
-</div>
-@endsection
+    <div class="main1">
+        <img class="gambar1" src="{{ asset('css/first.png') }}">
+        <p class="kata2"> SELAMAT DATANG DI WEBSITE TINGKAT KERAWANAN</p>
+        <p class="kata3"> HEPATITIS A DI KABUPATEN PACITAN</p>
+        <p class="kata4"> - HEPATITIC A RATE -</p>
+    </div>
+</body>
+
+</html>
