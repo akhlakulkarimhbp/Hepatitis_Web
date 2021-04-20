798f752268d2b6890587526e9ca879458da45b58
Akhlakul Karim HBP <akhlakulkarimhbp110214@gmail.com>
2021-04-20 19:33:24 +0700

    progres

--- resources/views/pages/datakriteria/index.blade.php
+++ resources/views/pages/datakriteria/index.blade.php
@@ -4,29 +4,30 @@
  <div class="content-header">
   <div class="container-fluid">
     <div class="row mb-2">
-      <div class="col-sm-12">
+      <div class="col-sm-9">
         <h1 class="m-0">Data Kriteria Hepatitis A</h1>
-            <div class="float-left my-3">
-            <a class="btn btn-success">Tambah Data</a>
-            </div>
+        <div class="float-left my-3">
+            <a class="btn btn-success"> Tambah Data</a>
+        </div>
         <table class="table table-striped table-hover table-sm table-bordered">
-			  <thead class="thead-light">
-				<tr>
-        <th>Id</th>
-        <!-- 
-					<th>Tahun</th>
-					<th>Kecamatan</th>
-        -->
-          <th>Rumah Sehat</th>
-          <th>Air Bersih</th>
-          <th>Penduduk</th>
-          <th>Kasus</th>
-          <th>Sanitasi</th>
-					<th>Aksi</th>
-				</tr>
-			</thead>
-      @include('sweetalert::alert')
+			    <thead class="thead-light">
+				    <tr>
+            <th>Id</th>
+            <!-- 
+            <th>Tahun</th>
+            <th>Kecamatan</th>
+             -->
+            <th>Air Bersih</th>
+            <th>Kasus</th>
+            <th>Penduduk</th>
+            <th>Sanitasi</th>
+            <th>Rumah Sehat</th>
+            <th>Aksi</th>
+            </tr>
+			    </thead>
+        </table>
       </div><!-- /.col -->
     </div><!-- /.row -->
   </div><!-- /.container-fluid -->
   @endsection
+
